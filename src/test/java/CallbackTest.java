76b2313937d04f3279cf62e3bcc29baf376113fd
sabibi_k <sabina7350@gmail.com>
2024-04-23 23:47:09 +0300

    Jar

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -1,4 +1,3 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -37,7 +37,7 @@ class CallbackTest {
     @Test
     void ValidDataTest() {
         driver.findElement(By.cssSelector("[type='text']")).sendKeys("Иванов Иван");
-        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79998887766");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79999999999");
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.cssSelector(".button__text")).click();
         String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
@@ -47,7 +47,7 @@ class CallbackTest {
     @Test
     void InvalidNameSurnameTest1() {
         driver.findElement(By.cssSelector("[type='text']")).sendKeys("Ivanov");
-        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79998887766");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79999999999");
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.cssSelector(".button__text")).click();
         String text = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
@@ -57,7 +57,7 @@ class CallbackTest {
     @Test
     void InvalidNameSurnameTest2() {
         driver.findElement(By.cssSelector("[type='text']")).sendKeys("");
-        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79998887766");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79999999999");
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.cssSelector(".button__text")).click();
         String text = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
@@ -67,7 +67,7 @@ class CallbackTest {
     @Test
     void EmptyCheckbox() {
         driver.findElement(By.cssSelector("[type='text']")).sendKeys("Иванов-Петров Иван");
-        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79998887766");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79999999999");
         driver.findElement(By.cssSelector(".button__text")).click();
         String text = driver.findElement(By.cssSelector("[data-test-id='agreement'].input_invalid .checkbox__text")).getText();
         assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", text.trim());
